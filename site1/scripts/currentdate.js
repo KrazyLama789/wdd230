4f15687b43795ebd6090bef74f29e0b430aa191f
KrazyLama789 <maryrgoff@gmail.com>
2022-09-17 17:21:35 -0600

    changed getElementById to querrySelector

--- site1/scripts/currentdate.js
+++ site1/scripts/currentdate.js
@@ -1,2 +1,2 @@
 const options = {weekday: 'short', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString("en-US", options);
+document.querySelector('#currentdate').textContent = new Date().toLocaleDateString("en-US", options);
