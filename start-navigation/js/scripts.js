72f4911352feb47a0f5592fc0166d5336d0198e3
KrazyLama789 <maryrgoff@gmail.com>
2022-10-08 12:24:50 -0600

    added other school folers in other

---
+++ start-navigation/js/scripts.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    document.querySelector("#primaryNav").classList.toggle("open");
+    document.querySelector("#hamburgerButton").classList.toggle("open");
+}
+
+const x = document.querySelector("#hamburgerButton");
+
+x.onclick = toggleMenu;
