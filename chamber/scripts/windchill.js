9a94b50697fd27bf484215f8f0e6a2ca2af905f7
KrazyLama789 <maryrgoff@gmail.com>
2022-10-23 00:22:31 -0600

    added a winchill conditional.

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,9 +1,15 @@
-document.querySelector(".temp span").textContent = 50;
-document.querySelector(".wind-speed").textContent = 7;
+const temp = 50;
+const speed = 7;
 
-const temp = document.querySelector(".temp span").textContent;
-const speed = document.querySelector(".wind-speed").textContent;
-const windchill = 35.74 + 0.6215 * temp - 35.75 * speed ** 0.16 + 0.4275 * temp * speed ** 0.16;
-
-console.log(temp);
-document.querySelector(".wind-chill").textContent = Math.round(windchill*10)/10;
+document.querySelector(".temp span").textContent = temp;
+document.querySelector(".wind-speed").textContent = speed;
+if (temp <= 50 && speed > 3) {
+  const windchill =
+    35.74 +
+    0.6215 * temp -
+    35.75 * speed ** 0.16 +
+    0.4275 * temp * speed ** 0.16;
+    
+  document.querySelector(".wind-chill").textContent =
+    Math.round(windchill * 10) / 10;
+}
