4b9a9d98bcd6873726818403a52e1ac4ce715dba
KrazyLama789 <maryrgoff@gmail.com>
2022-11-05 22:08:25 -0600

    added visit counter.

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -1,6 +1,7 @@
 // Timestamp of last updated.
 document.querySelector(".lastupdated").textContent = "Last Updated: " + document.lastModified;
 
+
 // Current year
 const date = new Date();
 const year = date.getFullYear();
@@ -19,3 +20,17 @@ const day = date.getDay();
 if (day == 1 || day == 2) {
   toggleBanner();
 }
+
+// Visit info.
+const visDisplay = document.querySelector(".visits");
+
+let numVis = Number(window.localStorage.getItem("visits-ls"));
+
+if (numVis !== 0) {
+  visDisplay.textContent = "Number of Visits: " + numVis;
+} else {
+  visDisplay.textContent = "Welcome, New User! ";
+};
+
+numVis++;
+localStorage.setItem("visits-ls", numVis); 
--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -3,5 +3,11 @@ function toggleMenu() {
   document.querySelector("#hamburger-button").classList.toggle("open");
 }
 
-const x = document.querySelector("#hamburger-button");
-x.onclick = toggleMenu;
+const ham = document.querySelector("#hamburger-button");
+ham.onclick = toggleMenu;
+
+const x = document.querySelector(".exit");
+
+x.addEventListener('click', () => {
+  document.querySelector("#last-visited").style.display = "none"
+});
