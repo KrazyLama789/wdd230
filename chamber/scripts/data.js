23f562c804d4f3f0c4901982d3feb001f8e68dc7
KrazyLama789 <maryrgoff@gmail.com>
2022-11-12 23:13:51 -0700

    adjusted directory cards

--- chamber/scripts/data.js
+++ chamber/scripts/data.js
@@ -15,13 +15,14 @@ function displayBusinesses(business) {
         const logo = document.createElement('img');
         const address = document.createElement('p');
         const phonenumber = document.createElement('p');
-        const weburl = document.createElement('p');
+        const weburl = document.createElement('a');
     
         // Set values to the new text elements
         company.textContent = business.name;
         address.textContent = business.address;
         phonenumber.textContent = business.phonenumber;
         weburl.textContent = business.weburl;
+        weburl.setAttribute('href', business.weburl);
     
         // Set values of logos and images
         logo.setAttribute('src', business.logoimage);
