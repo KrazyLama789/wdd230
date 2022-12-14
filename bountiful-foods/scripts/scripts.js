fec868895c3ceec22e406eeeb2438daba9eb680c
KrazyLama789 <Maryrgoff@gmail.com>
2022-12-13 23:22:29 -0700

    added dinamic location cards

--- bountiful-foods/scripts/scripts.js
+++ bountiful-foods/scripts/scripts.js
@@ -26,14 +26,14 @@ if (weathercard != null) {
 
   // Update Weather Info
   function displayWeather(weatherData) {
-    const weatherdiv = document.createElement('div');
-    weatherdiv.setAttribute('id', 'currweather');
+    const weatherdiv = document.createElement("div");
+    weatherdiv.setAttribute("id", "currweather");
 
     // Weather Icon
     const weathericon = document.createElement("img");
     const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
     weathericon.setAttribute("src", iconsrc);
-    weathericon.setAttribute('class', 'weathericon');
+    weathericon.setAttribute("class", "weathericon");
     weatherdiv.appendChild(weathericon);
 
     // Temp
@@ -58,8 +58,77 @@ if (weathercard != null) {
 
     weathercard.appendChild(weatherdiv);
     weathercard.appendChild(humidityelement);
-    weathercard.appendChild(document.createElement('hr'));
+    weathercard.appendChild(document.createElement("hr"));
     weathercard.appendChild(descriptionelement);
-    weathercard.appendChild(document.createElement('hr'));
+    weathercard.appendChild(document.createElement("hr"));
+  }
+}
+
+// Locations
+const locationsection = document.querySelector('#locations');
+
+if (locationsection != null) {
+  fetch("scripts/data.json")
+  .then((response) => response.json())
+  .then(function (jsonObject) {
+    const locationlist = jsonObject["locations"];
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+
+    locationlist.forEach(displayLocations);
+  });
+
+  function displayLocations(locationlist) {
+    const article = document.createElement('article');
+    article.setAttribute('class', 'card');
+
+    const heading = document.createElement('h3');
+    heading.textContent = locationlist.name;
+
+    const image = document.createElement('img');
+    image.setAttribute("src", locationlist.image);
+    image.setAttribute("alt", locationlist.imagedata);
+    image.setAttribute("loading", "lazy");
+
+    const paragraph = document.createElement('p');
+    paragraph.textContent = locationlist.info;
+
+    article.appendChild(heading);
+    article.appendChild(image);
+    article.appendChild(paragraph);
+    locationsection.appendChild(article);
+  }
+}
+
+// Images
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+if (imagesToLoad != null) {
+  const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px",
+  };
+
+  const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+      image.removeAttribute("data-src");
+    };
+  };
+  if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
   }
 }
