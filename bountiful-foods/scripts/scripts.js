fd0ade578cf16c490e9caaa6d28973bda2f0f64a
KrazyLama789 <Maryrgoff@gmail.com>
2022-12-14 18:13:15 -0700

    built out validation for custom drink

--- bountiful-foods/scripts/scripts.js
+++ bountiful-foods/scripts/scripts.js
@@ -1,4 +1,4 @@
-// Timestamp of last updated.
+// Timestamp of "Last Updated".
 document.querySelector(".lastupdated").textContent = document.lastModified;
 
 // Weather Information.
@@ -60,8 +60,8 @@ if (weathercard != null) {
     weathericon.setAttribute("alt", description);
 
     // Three Day Forecast
-    const heading = document.createElement('h3');
-    heading.textContent = 'Three Day forecast';
+    const heading = document.createElement("h3");
+    heading.textContent = "Three Day forecast";
     const threedaydiv = document.createElement("div");
     threedaydiv.setAttribute("id", "threeday");
 
@@ -69,17 +69,19 @@ if (weathercard != null) {
     weathercard.appendChild(weatherdiv);
     weathercard.appendChild(humidityelement);
     weathercard.appendChild(document.createElement("hr"));
-    weathercard.appendChild(heading)
+    weathercard.appendChild(heading);
     weathercard.appendChild(document.createElement("hr"));
-    weathercard.appendChild(threedaydiv)
+    weathercard.appendChild(threedaydiv);
 
     // Daily High Low
     const forecast = forecastData.list;
     for (let i = 0; i < forecast.length; i++) {
-      const dailytemp = document.createElement('p');
-      dailytemp.textContent = `High ${Math.round(forecast[i].main.temp_max)}/${Math.round(forecast[i].main.temp_min)} Low`;
+      const dailytemp = document.createElement("p");
+      dailytemp.textContent = `High ${Math.round(
+        forecast[i].main.temp_max
+      )}/${Math.round(forecast[i].main.temp_min)} Low`;
       threedaydiv.appendChild(dailytemp);
-    };
+    }
   }
 }
 
@@ -118,7 +120,7 @@ if (locationsection != null) {
   }
 }
 
-// Images
+// Placeholders to Images
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 if (imagesToLoad != null) {
@@ -149,5 +151,93 @@ if (imagesToLoad != null) {
     imagesToLoad.forEach((img) => {
       loadImages(img);
     });
+  }
+}
+
+// Drink Form
+const ing = document.querySelector("#ing");
+
+if (ing != null);
+{
+  fetch("scripts/data.json")
+    .then((response) => response.json())
+    .then(function (jsonObject) {
+      const ingredients = jsonObject["ingredients"];
+      // console.table(jsonObject); // temporary checking for valid response and data parsing
+
+      ingredients.forEach(displayIngredients);
+    });
+
+  function displayIngredients(ingredients) {
+    const label = document.createElement("label");
+    label.setAttribute("class", "sbs");
+    label.textContent += `${ingredients.name} `;
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("name", "checkbox");
+    input.setAttribute("value", ingredients.number);
+    input.setAttribute("onclick", `verify(${ingredients.number})`);
+
+    label.appendChild(input);
+    ing.appendChild(label);
+  }
+
+  function verify(number) {
+    const checked = document.querySelectorAll("input:checked");
+
+    if (checked.length === 0) {
+      // there are no checked checkboxes
+      console.log("no checkboxes checked");
+    } else if (checked.length > 3) {
+      alert("Please select only 3 ingredients");
+      document.querySelector(`[value="${number}"]`).checked = false;
+    }
+  }
+
+  function validateForm() {
+    if (document.querySelectorAll("input:checked").length !== 3) {
+      alert("please select 3 ingredients");
+      return false;
+    } else {
+      displaySubmition();
+      return false;
+    }
+  }
+
+  function displaySubmition() {
+    // Section
+    const mix = document.querySelector('.mix');
+
+    const name = document.createElement('p');
+    name.textContent = document.querySelector('input[name="fname"]').value;
+    mix.appendChild(name);
+
+    const email = document.createElement('p');
+    email.textContent = document.querySelector('input[name="email"]').value;
+    mix.appendChild(email);
+
+    const phone = document.createElement('p');
+    phone.textContent = document.querySelector('input[name="phone"]').value;
+    mix.appendChild(phone);
+
+    const instruct = document.createElement('p');
+    instruct.textContent = document.querySelector('input[name="instruct"]').value;
+    mix.appendChild(instruct);
+
+    // List of checked items
+    const ings = document.querySelectorAll('input:checked');
+    ings.forEach((ing) => {
+      ingredient = document.createElement('p')
+      ingredient.textContent = ing.value;
+      mix.appendChild(ingredient);
+    });
+
+    // the input values of the order (7 inputs = first name, email, 
+    // phone, three selected fruits, and special instructions),
+    // the order date, and
+    // the total amount of carbohydrates, protein, fat, sugar, and calories 
+    // based upon the three fruit choices selected on the form. Again, use the JSON fruit 
+    // data to extract and sum up that information.
   }
 }
